c694212c676e612713fdfac40f4b96d26a4b647e
csi <csi@Agrihack0x05.2jawnzuaw5cejekq1xifl3dtze.ix.internal.cloudapp.net>
2020-10-25 12:15:45 +0000

    update

--- Reverse/C-0x00/c-0x00.c
+++ Reverse/C-0x00/c-0x00.c
@@ -2,7 +2,7 @@
 #include <string.h>
 #include <stdlib.h>
 
-unsigned int arr[24] = {230, 230, 230, 144, 144, 144, 146, 146, 146, 204, 204, 204, 232, 232, 232, 146, 146, 146, 156, 156, 156, 206, 206, 206};
+unsigned int arr[20] = {238, 202, 216, 198, 222, 218, 202, 190, 232, 222, 190, 228, 202, 236, 202, 228, 230, 210, 220, 206};
 
 int main(int argc, char const *argv[])
 {
@@ -13,13 +13,13 @@ int main(int argc, char const *argv[])
 
 	scanf("%32s", buf);
 
-	if(strlen(buf)!=24){
+	if(strlen(buf)!=20){
 		puts("invalid length.");
 		exit(0);
 	}
 
-	while(i < 24){
-		if(((int)buf[i] << 1) != arr[i]){
+	while(i < 20){
+		if(((int)buf[i]*2) != arr[i]){
 			puts("nope.");
 			exit(0);
 		}
