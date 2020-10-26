e6e540dc5fd26ef33f14f8963644e457043e1926
csi <csi@Agrihack0x05.2jawnzuaw5cejekq1xifl3dtze.ix.internal.cloudapp.net>
2020-10-26 03:48:11 +0000

    add exec baby rev

--- Reverse/baby-C-0x00/baby-c-0x00.c
+++ Reverse/baby-C-0x00/baby-c-0x00.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+#include <string.h>
 
-int int main(int argc, char const *argv[])
+int main(int argc, char const *argv[])
 {
     char s[40];
     printf("input: ");
     scanf("%32s", s);
     if(!strcmp(s, "31337")){
-        printf("Congratz!\nHere is your flag: agrihack{welcome_to_baby_c_0x??_series}");
+        printf("Congratz!\nHere is your flag: agrihack{yeah_this_is_easier_than_C_0x??_series}\n");
     }
     return 0;
 }
