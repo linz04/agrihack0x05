a995b2f943810efb8246b349d1fb4a07bc8818b0
L29 <59005540+L29@users.noreply.github.com>
2020-11-28 23:56:49 +0700

    Delete book2.php

--- final/Web/insecure/LSI3/src/book2.php
+++
@@ -1,3 +0,0 @@
-<head><title>Enola Holmes</title></head>
-<h1>Enola Holmes</h1>
-<p>While searching for her missing mother, intrepid teen Enola Holmes uses her sleuthing skills to outsmart big brother Sherlock and help a runaway lord.
