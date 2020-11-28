49597d6fcffb30e6ecfa3478fab948d487aea84c
L29 <59005540+L29@users.noreply.github.com>
2020-11-29 00:46:28 +0700

    Add files via upload

--- final/Binary/Global/global.c
+++ final/Binary/Global/global.c
@@ -2,9 +2,8 @@
 #include <string.h>
 #include <stdlib.h>
 
-//gcc global.c -no-pie -fno-stack-protector -o global
-
-char *string="cat flag.txt";
+//gcc global.c -fno-stack-protector -o global
+char flag[64];
 
 void logo(){
   puts("██╗     ██╗███╗   ██╗███████╗");
@@ -16,23 +15,33 @@ void logo(){
 }
 
 
-void setup()
-{
-  setvbuf(stdin, 0, 2, 0);
-  setvbuf(stdout, 0, 2, 0);
+
+void setup(){
+  setvbuf(stdin,0,2,0);
+  setvbuf(stdout,0,2,0);
+  FILE *fp = fopen("flag.txt","r");
+  if(fp == NULL){
+    printf("No file flag.txt\n");
+    exit(0);
+  }
+  fgets(flag,60,fp);
 }
 
+void linz_here(){
+	printf("%s\n", flag);
+}
+
+int vuln(){
+	setup();
+	logo();
+	char buff[32];
+	puts("In This challenge PIE is ENABLE");
+	puts("Dont worry it still solveable");
+	printf("Input : ");
+	read(0,buff,80);
+}
 
 int main(){
-	char s[32];
-
-	setup();
-
-	do{
-		logo();
-		printf("Input [yes/no]: ");
-		gets(s);
-	}while(!strcmp(s, "yes"));
-
+	vuln();
 	return 0;
 }
