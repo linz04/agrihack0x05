c4bfbe73f60f0bfb09ca1822be886e175f31624a
L29 <59005540+L29@users.noreply.github.com>
2020-11-29 09:20:02 +0700

    Add files via upload

---
+++ final/Binary/String2/src/fmt.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+//gcc fmt.c -no-pie -m32 -fno-stack-protector -o fmt
+
+int target = 0x1337;
+
+void logo(){
+  puts("██╗     ██╗███╗   ██╗███████╗");
+  puts("██║     ██║████╗  ██║╚══███╔╝");
+  puts("██║     ██║██╔██╗ ██║  ███╔╝ ");
+  puts("██║     ██║██║╚██╗██║ ███╔╝  ");
+  puts("███████╗██║██║ ╚████║███████╗");
+  puts("╚══════╝╚═╝╚═╝  ╚═══╝╚══════╝");
+}
+
+void setup()
+{
+  setvbuf(stdin, 0, 2, 0);
+  setvbuf(stdout, 0, 2, 0);
+}
+
+
+int main(int argc, char **argv){
+
+  setup();
+  char buffer[64];
+  fgets(buffer, sizeof(buffer), stdin);
+  printf(buffer);
+  if(target == 0x7331){
+    system("cat flag.txt");
+  }
+  else{
+    printf("sorry target still %x\n",target);
+  }
+  return 0;
+}
